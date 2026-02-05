cc8d68aa0e1a7a15fabdbdeda11aa3a52075fdec
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2026-02-05 18:23:42 +0545

    Quartz sync: Feb 5, 2026, 6:23 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "realsudarshan.github.io/obsidian_note",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
