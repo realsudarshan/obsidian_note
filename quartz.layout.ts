e4f645df06ab478cc35a52f8f8dc960f71aa622e
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2026-03-21 02:14:55 +0545

    Quartz sync: Mar 21, 2026, 2:14 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/realsudarshan",
-      "My Notes": "https://sudarshandhakal.com.np",
+      "Portfolio": "https://sudarshandhakal.com.np",
     },
   }),
 }
